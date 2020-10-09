df4fc821ea05ca98a30b5dc1dcc60ebf0e4378e7
tushar <41532632+vicuna1512@users.noreply.github.com>
2020-10-09 16:38:41 +0530

    Updates

---
+++ precache-manifest.e546ef65032e96e693218eae31c00ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a389ae5a003791fb2c08881109839854",
+    "url": "/sleepEon/index.html"
+  },
+  {
+    "revision": "e71bdab3d92f4497e160",
+    "url": "/sleepEon/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "691955d0c4600ae6c11a",
+    "url": "/sleepEon/static/js/2.9909fd95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sleepEon/static/js/2.9909fd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71bdab3d92f4497e160",
+    "url": "/sleepEon/static/js/main.0a7af028.chunk.js"
+  },
+  {
+    "revision": "512bf1c4035b12e91248",
+    "url": "/sleepEon/static/js/runtime-main.78699b74.js"
+  }
+]);
